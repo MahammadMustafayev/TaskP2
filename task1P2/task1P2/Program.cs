c004c5a24e4a382e0a2eef809f3a8028afc35390
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-03-04 23:48:37 +0300

    added

---
+++ task1P2/task1P2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task1P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 6, 3, 1, 7, 8, 5 };
+            int small = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (small>nums[i])
+                {
+                    small = nums[i];
+                }
+                //else if (small<nums[i])
+                //{
+                //    Console.WriteLine("Bu en boyuk elementidir" );
+                //} 
+            }
+            Console.WriteLine("Bu en kicik elementdir" + small);
+        } 
+    }
+}
