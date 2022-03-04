23689c61c4a9e285e6f522001f7156c8f426dc48
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-03-05 01:10:14 +0300

    added

---
+++ task2P2/task2P2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task2P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+            int count = 0;
+            char character = 'a'  ;
+
+                foreach (char ch in names)
+                {
+                    if (ch == character)
+                    {
+                        count++;
+                    }
+                    
+                }
+                Console.WriteLine("Bu mətndə (a) simvolunun sayı:" + count);
+            
+           
+            
+        }
+       
+    }
+}
